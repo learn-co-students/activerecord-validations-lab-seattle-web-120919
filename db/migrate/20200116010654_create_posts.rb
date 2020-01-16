274b9212355189aedb446164b3a16576c1ac43ab
github username <github email address>
2020-01-15 18:17:23 -0800

    Done.

--- db/migrate/20200116010654_create_posts.rb
+++ db/migrate/20200116010654_create_posts.rb
@@ -1,11 +1,11 @@
-class CreatePosts < ActiveRecord::Migration
+class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :title
       t.text :content
       t.text :summary
       t.string :category
-
+  
       t.timestamps null: false
     end
   end
